99737214c6006ffcb77aa3e486254e03fc995186
AmineBma <gm_benbakhta@esi.dz>
2018-03-14 18:57:15 +0100

    TD 3 Exercice 4 : Collections & Exceptions

---
+++ src/bma/amine/Pile.java
@@ -0,0 +1,23 @@
+package bma.amine;
+
+public class Pile {
+    public void empiler(int x)
+    {
+
+    }
+
+    public int depiler()
+    {
+        return 0;
+    }
+
+    public int sommet()
+    {
+        return 0;
+    }
+
+    public boolean estVide()
+    {
+        return false;
+    }
+}
